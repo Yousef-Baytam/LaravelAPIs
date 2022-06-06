0f7bcd86c4076df9808b466d2aa33633978d2eed
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-06 15:07:05 +0300

    Created a class method to check palindroms

--- app/Http/Controllers/assignmentController.php
+++ app/Http/Controllers/assignmentController.php
@@ -6,4 +6,18 @@ use Illuminate\Http\Request;
 
 class assignmentController extends Controller
 {
+    public $arr = ['mamamiya', 'hello', 'abba'];
+
+    public function findPalindroms()
+    {
+        $final = [];
+        $count = 0;
+        foreach ($this->arr as $i) {
+            if (strrev($i) === $i) {
+                $count++;
+                array_push($final, $i);
+            }
+        }
+        return response()->json(['palindrom count' => $count, 'palindroms' => $final], 200);
+    }
 }
